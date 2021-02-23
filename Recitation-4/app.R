7acf134964e2515ad4c3ec345154d0953262997b
Your Name <nanakorantema_koranteng@fas.harvard.edu>
2021-02-23 14:36:51 -0500

    orange county map

--- Recitation-4/app.R
+++ Recitation-4/app.R
@@ -39,8 +39,30 @@ ui <- navbarPage(
              p("My name is Nana-Korantema Koranteng and I study the Middle East. 
              You can reach me at nanakorantema_koranteng@g.harvard.edu.")))
 # Define server logic required to draw a histogram
-server <- function(input, output) {}
-
+server <- function(input, output) {
+  racevars <- c(White = "B02001_002", 
+                Black = "B02001_003", 
+                Asian = "B02001_005",
+                Hispanic = "B03003_003")
+  Orange <- get_acs(geography = "tract",
+                    variables = racevars, 
+                    year = 2018,
+                    state = "CA",
+                    county = "Orange County",
+                    geometry = TRUE,
+                    summary_var = "B02001_001") 
+  output$map <- renderPlot({Orange %>%
+      mutate(Percent = 100 * (estimate / summary_est)) %>%
+      ggplot(aes(fill = Percent, color = Percent)) +
+      facet_wrap(~ variable) +
+      geom_sf() +
+      scale_fill_viridis_c(direction = -1) +
+      scale_color_viridis_c(direction = -1) +
+      labs(title = "Racial geography of Orange County, California",
+           caption = "Source: American Community Survey 2014-2018") +
+      theme_void()
+  })
+}
 
 
 
